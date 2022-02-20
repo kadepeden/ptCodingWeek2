b2cfbdddd63d9a14ef659f7eb43cc0472eb2d0cb
kadepeden <kadepeden@gmail.com>
2022-02-20 12:24:33 -0800

    week 2 loop practice

---
+++ week2.js
@@ -0,0 +1,18 @@
+for (let i = 0; i < 10; i++ ) {
+    console.log (i)
+} 
+
+let a = 110;
+
+while (a < 10) {
+    console.log(a);
+    a++;
+}
+
+do {
+    console.log(a);
+} while (a < 10);
+
+for (i in names) {
+    console.log(names[i])
+}
